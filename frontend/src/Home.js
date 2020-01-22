2b18aca447f3cf86768a948dc75b32b9cb2f746a
Nick <nst447@gmail.com>
2020-01-22 12:41:14 -0800

    Overhauled layout of home component

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,21 +1,45 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Row, Col, Image, Button } from "react-bootstrap";
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
+      <Row className="my-5">
         {!(window.localStorage._token) ?
-          <Link to="/login"><button>Login</button></Link> :
-          <div>
-            <h3>Welcome Back!</h3>
-            <p>LETS GET ONLINE</p>
-          </div>
+          <Col>
+            <Row className="my-3">
+              <Col>
+                <Link to="/login"><Button variant="info" size="lg">Login</Button></Link>
+              </Col>
+            </Row>
+            <Row className="my-3">
+              <Col>
+                {officeImage}
+              </Col>
+            </Row>
+          </Col>
+          :
+          <Col xs={12}>
+            <Row className="my-3">
+              <Col>
+                <h3>Welcome Back!</h3>
+              </Col>
+            </Row>
+            <Row className="my-3">
+              <Col>
+                {officeImage}
+              </Col>
+            </Row>
+          </Col>
         }
-      </div>
+      </Row>
     );
   }
 }
+
+const officeImage = (
+  <Image src="/cartoon-office.jpg" fluid rounded />
+)
 
 export default Home;
