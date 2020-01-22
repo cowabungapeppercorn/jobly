00d9a071e7311d6190f18ed6043caf9912ee1bf9
Nick <nst447@gmail.com>
2020-01-22 12:50:12 -0800

    Added react-bootstrap to register form

--- frontend/src/RegisterForm.js
+++ frontend/src/RegisterForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
+import { Form, Button, Col, Row } from "react-bootstrap";
 
 
 class RegisterForm extends Component {
@@ -31,60 +32,117 @@ class RegisterForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div>
-          <label htmlFor="username"></label>
-          <input
-            onChange={this.handleChange}
-            placeholder="Username"
-            type="text"
-            name="username"
-            id="username"
-            value={this.state.username}
-          />
-        </div>
-        <div>
-          <label htmlFor="password"></label>
-          <input
-            onChange={this.handleChange}
-            placeholder="Password"
-            type="password"
-            name="password"
-            value={this.state.password}
-          />
-        </div>
-        <div>
-          <label htmlFor="first_name"></label>
-          <input
-            onChange={this.handleChange}
-            placeholder="First Name"
-            type="text"
-            name="first_name"
-            value={this.state.first_name}
-          />
-        </div>
-        <div>
-          <label htmlFor="last_name"></label>
-          <input
-            onChange={this.handleChange}
-            placeholder="Last Name"
-            type="text"
-            name="last_name"
-            value={this.state.last_name}
-          />
-        </div>
-        <div>
-          <label htmlFor="email"></label>
-          <input
-            onChange={this.handleChange}
-            placeholder="email"
-            type="text"
-            name="email"
-            value={this.state.email}
-          />
-        </div>
-        <button>Register</button>
-      </form>
+      <Row>
+        <Col lg={{ span: 6, offset: 3 }}>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label>username</Form.Label>
+              <Form.Control
+                onChange={this.handleChange}
+                placeholder="username"
+                type="text"
+                name="username"
+                value={this.state.username}
+              />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>password</Form.Label>
+              <Form.Control
+                onChange={this.handleChange}
+                placeholder="password"
+                type="password"
+                name="password"
+                value={this.state.password}
+              />
+            </Form.Group>
+            <Form.Group controlId="first_name">
+              <Form.Label>first name</Form.Label>
+              <Form.Control
+                onChange={this.handleChange}
+                placeholder="first_name"
+                type="text"
+                name="first_name"
+                value={this.state.first_name}
+              />
+            </Form.Group>
+            <Form.Group controlId="last_name">
+              <Form.Label>last name</Form.Label>
+              <Form.Control
+                onChange={this.handleChange}
+                placeholder="last_name"
+                type="text"
+                name="last_name"
+                value={this.state.last_name}
+              />
+            </Form.Group>
+            <Form.Group controlId="email">
+              <Form.Label>email</Form.Label>
+              <Form.Control
+                onChange={this.handleChange}
+                placeholder="email"
+                type="text"
+                name="email"
+                value={this.state.email}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">Submit</Button>
+          </Form>
+        </Col>
+      </Row>
+      // <form onSubmit={this.handleSubmit}>
+      //   <div>
+      //     <label htmlFor="username"></label>
+      //     <input
+      //       onChange={this.handleChange}
+      //       placeholder="Username"
+      //       type="text"
+      //       name="username"
+      //       id="username"
+      //       value={this.state.username}
+      //     />
+      //   </div>
+      //   <div>
+      //     <label htmlFor="password"></label>
+      //     <input
+      //       onChange={this.handleChange}
+      //       placeholder="Password"
+      //       type="password"
+      //       name="password"
+      //       value={this.state.password}
+      //     />
+      //   </div>
+      //   <div>
+      //     <label htmlFor="first_name"></label>
+      //     <input
+      //       onChange={this.handleChange}
+      //       placeholder="First Name"
+      //       type="text"
+      //       name="first_name"
+      //       value={this.state.first_name}
+      //     />
+      //   </div>
+      //   <div>
+      //     <label htmlFor="last_name"></label>
+      //     <input
+      //       onChange={this.handleChange}
+      //       placeholder="Last Name"
+      //       type="text"
+      //       name="last_name"
+      //       value={this.state.last_name}
+      //     />
+      //   </div>
+      //   <div>
+      //     <label htmlFor="email"></label>
+      //     <input
+      //       onChange={this.handleChange}
+      //       placeholder="email"
+      //       type="text"
+      //       name="email"
+      //       value={this.state.email}
+      //     />
+      //   </div>
+      //   <button>Register</button>
+      // </form>
     );
   }
 }
