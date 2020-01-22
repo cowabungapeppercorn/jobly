8f0357d0580c6ff7e1d0e1eb430e65ea1e78cc0d
Nick <nst447@gmail.com>
2020-01-22 11:58:46 -0800

    Minor changes to nav styling

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -4,13 +4,13 @@ import { NavLink } from "react-router-dom";
 class Nav extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light px-5">
         <NavLink className="navbar-brand" to="/">Jobly</NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <NavLink className="nav-link" to="/companies">Companies <span className="sr-only">(current)</span></NavLink>
             </li>
